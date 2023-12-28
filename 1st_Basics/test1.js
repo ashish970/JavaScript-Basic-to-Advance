26886cc5584954fb9ef044a93ad282a665346533
Ashish <ashish9gps@gmail.com>
2023-12-28 17:57:17 +0000

    container config done

---
+++ 1st_Basics/test1.js
@@ -0,0 +1,7 @@
+console.log("Ashish kumar")
+
+console.log("This is the first part of JavaScript language");
+
+console.log("Now from this repository of this GitHub the series of JavaScript will begin");
+
+console.log("Hello Javascript")
