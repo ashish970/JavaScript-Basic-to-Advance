e91076b0bc1813866f5dd67bdedab8d9207c7919
Ashish <ashish9gps@gmail.com>
2024-01-02 10:45:21 +0000

    datatypes

---
+++ 1st_Basics/02_datatypes.js
@@ -0,0 +1,50 @@
+"use strict";   //treat all JS code as newer version
+
+// alert(3 + 3)
+
+/* this alert will work on chrome or any other Engine of browser but 
+not work here............because of node environment......to write
+this alert there is a separate syntax for this. 
+......we are using NodeJs not browser.*/
+
+console.log(3 +
+     3);          //thi is not the write way to write code,  
+                  //code readibility should be first priority
+
+
+
+console.log("Ashish") 
+
+console.log("hey")
+console.log('hey nobbie')
+
+
+  // now the topic Datatypes
+
+  let name = "Ashish Kumar"     //string datatype
+  let age = 23         //number datatype
+  let isLoggedIn = "true" / "false"    //boolean dataypes
+
+  console.log(typeof "Ashish");
+
+
+
+  //types of datatypes
+  //all of these are primitive datatypes
+
+  //number     => range 2 to power 53  
+  //BigInt     =>
+  //String     => '' && ""  .......but always prefer ""
+  //Boolean    => true/false
+  //null       => standalone value
+  //undefined  => abhi value assign nhi hue hai
+  //symbol     => it is uses whenever comes to be uniqueness
+  //mostly symbol uses in React or Figma
+
+  
+  //object
+
+  console.log(typeof undefined); //undefined is a type
+  console.log(typeof null);  //here it shows object it's an error in JavaScript
+  
+
