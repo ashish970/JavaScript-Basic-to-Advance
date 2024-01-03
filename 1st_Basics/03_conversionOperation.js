178a4704ced0766d27ef2bcf710754387487de68
Ashish <ashish9gps@gmail.com>
2024-01-03 19:24:12 +0000

    final updated

---
+++ 1st_Basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+//conversionOperation in JavaScript
+ let score = "1"
+
+ console.log(typeof score);
+ console.log(typeof (score));    //the reason of prenthesis it's shows method
+
+ let valueInNumber =Number(score)
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+
+
+// more on conversion
+
+//let isLoggedIn = 1
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
