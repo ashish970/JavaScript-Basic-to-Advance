166716874f2b5c191fdb8b6641ad822d701b5de0
Ashish <ashish9gps@gmail.com>
2024-01-08 18:13:25 +0000

    conversionoperations

--- 1st_Basics/03_conversionOperation.js
+++ 1st_Basics/03_conversionOperation.js
@@ -1,19 +1,28 @@
 //conversionOperation in JavaScript
- let score = "1"
+ //let score = "1"
 
- console.log(typeof score);
- console.log(typeof (score));    //the reason of prenthesis it's shows method
+ //console.log(typeof score);
+ //console.log(typeof (score));    //the reason of prenthesis it's shows method
 
- let valueInNumber =Number(score)
- console.log(typeof valueInNumber);
- console.log(valueInNumber);
+ //let valueInNumber =Number(score)
+ //console.log(typeof valueInNumber);
+ //console.log(valueInNumber);
 
 
 // more on conversion
 
-//let isLoggedIn = 1
+//let isLoggedIn = "ashish"
 
 //let booleanIsLoggedIn = Boolean(isLoggedIn)
+
 //console.log(booleanIsLoggedIn);
 
+//notes
+// 1 => true;  0 => false
+//"" => false
+//"Ashish" => true
 
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
