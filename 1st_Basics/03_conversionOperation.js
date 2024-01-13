51d730e4ef22cc8b60c1aa113f5aca0175a78ff9
Ashish <ashish9gps@gmail.com>
2024-01-13 10:37:55 +0000

    container config done

---
+++ 1st_Basics/03_conversionOperation.js
@@ -0,0 +1,101 @@
+//conversionOperation in JavaScript
+ //let score = "1"
+
+ //console.log(typeof score);
+ //console.log(typeof (score));    //the reason of prenthesis it's shows method
+
+ //let valueInNumber =Number(score)
+ //console.log(typeof valueInNumber);
+ //console.log(valueInNumber);
+
+
+// more on conversion
+
+//let isLoggedIn = "ashish"
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+//notes
+// 1 => true;  0 => false
+//"" => false
+//"Ashish" => true
+
+//let someNumber = 33
+//let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//*******************operations*****************//
+
+let value = 46
+let negativeValue = -value
+//console.log(negativeValue);
+
+
+//basic maths operation in js
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%3);
+
+//adding strings
+
+let str1 = "Hello"
+let str2 = " Ashish"
+let str3 = " How are you"
+console.log(str1 + str2 + str3);
+
+
+//now the complex part is :-
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2 );
+// console.log(1 + 2 + "2");
+
+//console.log((3 + 4) * 5 % 3);
+
+//console.log(+true);   //this is not the write way to write code.
+
+
+//why to write this type of confusion code...not a good practise
+let num1, num2, num3
+num1 = num2 = num3 = 3 + 4
+//console.log(num1,num2,num3); 
+
+
+let gameCounter = 9
+++gameCounter
+//console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+//console.log(`x:${x}, y:${y}`);
+
+//postfix expresiions
+let a = 3;
+const b = a++;
+// console.log(a);
+// console.log(b);
+
+
+//prefix expression
+let c = 4;
+const d = ++c;
+//console.log(c);
+//console.log(d);
+
+
+
+
+
+//this is all about conversionOperation in JavaScript
+
+
+
+
