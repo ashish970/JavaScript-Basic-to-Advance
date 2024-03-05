7ddaa8eb6410dbea9c01cba803f56ebe3fb960e9
Ashish <ashish9gps@gmail.com>
2024-03-05 19:58:06 +0530

    18_switch.js
    This file include switch concept in javascript
    final commit

---
+++ 1st_Basics/18_switch.js
@@ -0,0 +1,57 @@
+//In this file module we are going to study switch...
+
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 4
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
+
+const months = "January"
+
+switch (month) {
+    case "January":
+        console.log("January");
+        break;
+    case "February":
+        console.log("February");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
+//this is all about switch statement in js
